e0c6370a67229c527c7e46924e9381346c46a6a7
Leonardo <leonardoa.dev@gmail.com>
2024-10-20 19:40:19 -0300

    chore: authenticate org use case

---
+++ src/use-cases/authenticate-org.spec.ts
@@ -0,0 +1,53 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { AuthenticateOrgUseCase } from './authenticate-org'
+import { makeOrg } from 'test/factories/make-org'
+import { hash } from 'bcrypt'
+import { UnauthorizedError } from './errors/unauthorized-error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: AuthenticateOrgUseCase
+
+describe('Authenticate Org Use Case', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+
+    sut = new AuthenticateOrgUseCase(orgsRepository)
+  })
+
+  it('should be able to authenticate an org', async () => {
+    const password = '123456ab'
+    const passwordHash = await hash(password, 6)
+
+    const org = await orgsRepository.create({
+      ...makeOrg(),
+      passwordHash,
+    })
+
+    const { org: authenticatedOrg } = await sut.execute({
+      email: org.email,
+      password,
+    })
+
+    expect(authenticatedOrg.id).toEqual(org.id)
+  })
+
+  it('should not be able authenticate nonexistent org', async () => {
+    await expect(
+      sut.execute({ email: 'johndoe@email.com', password: '123456' }),
+    ).rejects.toBeInstanceOf(UnauthorizedError)
+  })
+
+  it('should not be able authenticate org with wrong password', async () => {
+    const password = '123456ab'
+    const passwordHash = await hash(password, 6)
+
+    const org = await orgsRepository.create({
+      ...makeOrg(),
+      passwordHash,
+    })
+
+    await expect(
+      sut.execute({ email: org.email, password: '123456' }),
+    ).rejects.toBeInstanceOf(UnauthorizedError)
+  })
+})
---
+++ src/use-cases/authenticate-org.ts
@@ -0,0 +1,39 @@
+import type { Org } from '@prisma/client'
+import { compare } from 'bcrypt'
+
+import type { OrgsRepository } from '@/repositories/orgs-repository'
+import { UnauthorizedError } from './errors/unauthorized-error'
+
+interface AuthenticateOrgUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateOrgUseCaseResponse {
+  org: Org
+}
+
+export class AuthenticateOrgUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateOrgUseCaseRequest): Promise<AuthenticateOrgUseCaseResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new UnauthorizedError()
+    }
+
+    const doesPasswordMatchWithHash = await compare(password, org.passwordHash)
+
+    if (!doesPasswordMatchWithHash) {
+      throw new UnauthorizedError()
+    }
+
+    return {
+      org,
+    }
+  }
+}
